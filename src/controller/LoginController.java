85e95afa47ab60106ebb52a40f095b1f3d697d40
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-11 10:22:22 +0530

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -16,18 +16,12 @@ import model.User;
 @WebServlet(urlPatterns= {"/login"})
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public LoginController() {
         super();
-        // TODO Auto-generated constructor stub
+      
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
 	rd.forward(request, response);
@@ -35,8 +29,8 @@ public class LoginController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("email"); //  get the email value from the jsp/html page
-		String password = request.getParameter("password"); //  get the password value from the jsp/html page
+		String email = request.getParameter("email"); 
+		String password = request.getParameter("password"); 
 
 		ValidateUser validateUser = new ValidateUser();
 		if(validateUser.validate(email,password)) {
