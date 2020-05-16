7c5a5dee20dd8ed63882eac286f9f97f4a5381c5
root <root@DESKTOP-7OS8CPN.localdomain>
2020-05-16 22:59:54 +0530

    YashKhanna 1527915

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Blog;
+import model.User;
 
 
 
@@ -32,11 +36,17 @@ public class BlogController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
+		String blogDetails = request.getParameter("selectedAnswers");
 		
+		String[] blogDetail=blogDetails.split(",");
 		
+		LocalDateTime ld1 = LocalDateTime.of(2005, 4, 9, 22, 05, 12);
+		User user = new User ( "amin@gmail.com","Amin@556",ld1);
 		
-
+		LocalDate ld=LocalDate.of(2020,03,23);
+		
+		Blog blog=new Blog(blogDetail[0], blogDetail[1], ld);
+		System.out.println("Blog Title: "+blogDetail[0]+"."+"Blog Description"+blogDetail[1]+"Posted On: "+ld);
 		
 		if(blog!=null) {
 			request.setAttribute("blog", blog);
@@ -44,7 +54,7 @@ public class BlogController extends HttpServlet {
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+		
 	}
 
 }
--- src/model/Blog.java
+++ src/model/Blog.java
@@ -1,2 +1,37 @@
 package model;
+
+import java.time.LocalDate;
+
 // Type your code
+
+public class Blog{
+	String title;
+	String description;
+	LocalDate postedOn;
+	public String getBlogTitle() {
+		return title;
+	}
+	public void setBlogTitle(String title) {
+		this.title = title;
+	}
+	public String getBlogDescription() {
+		return description;
+	}
+	public void setBlogDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getDate() {
+		return postedOn;
+	}
+	public void setDate(LocalDate postedOn) {
+		this.postedOn = postedOn;
+	}
+	public Blog(String title, String description, LocalDate postedOn) {
+		this.title = title;
+		this.description = description;
+		this.postedOn = postedOn;
+	}
+	
+	
+	
+}
